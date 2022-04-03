0345397a51e6c56eaf158a91e41277d1129b2c49
jyliu17 <jyliu17@gmail.com>
2022-04-03 17:00:02 -0400

    removed hello world

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1> Hello World</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import HeaderOne from './components/Header'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: HeaderOne
 }
 </script>
 

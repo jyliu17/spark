8ee5e1591ebeeac6a6f026622658990942203e4c
jyliu17 <jyliu17@gmail.com>
2022-04-03 16:58:23 -0400

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <h1>Fetched Object</h1>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: 'HeaderOne',
+    props: {
+
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+
+</style>
